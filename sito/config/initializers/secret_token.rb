f3d33ba43772a55146d17d6e197d17dff943f0e2
lucanata <luca.natali@student.unife.it>
2013-11-22 14:40:05 +0100

    Creazione del progetto in Rails

---
+++ sito/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sito::Application.config.secret_key_base = '01078a8091b86b4c9cf166bff842d3ce1107cd38bc9480b035dae6ab2437373d76502c97e70a2599f2553cef32efb46b99c42d32193efdac445d2acbd2bf56c4'
---
+++ sito/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Sito::Application.config.session_store :cookie_store, key: '_sito_session'
