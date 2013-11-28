279ac120de2165daf3568cce82cd6f9019e3ec7f
lucanata <luca.natali@student.unife.it>
2013-11-28 09:14:16 +0100

    Sistemato form sign_up con ridirezione e visualizzazione errori e sistemato tasti menù

--- sito/app/controllers/registration_controller.rb
+++ sito/app/controllers/registration_controller.rb
@@ -1,40 +1,59 @@
 class RegistrationController < Devise::RegistrationsController
 
-def new
+    def new
 
-@user= User.new
+        @user= User.new
 
+    end
+
+    def create
+
+    @user = User.new
+    @user.nome = params[:user][:nome]
+    @user.cognome = params[:user][:cognome]
+    #@user.datanascita = params[:user][:datanascita]
+    years=params[:user]['datanascita(1i)'];
+    month=params[:user]['datanascita(2i)'];
+    days=params[:user]['datanascita(3i)'];
+    d=Date.new(years.to_i,month.to_i,days.to_i)
+    @user.datanascita = d#params[:user][:datanascita]
+
+    @user.paese = params[:user][:paese]
+    @user.prov = params[:user][:prov]
+    @user.cap = params[:user][:cap]
+    @user.via = params[:user][:via]
+    @user.ncivico = params[:user][:ncivico]
+    @user.piva_codf = params[:user][:piva_codf]
+    @user.email = params[:user][:email]
+    @user.password = params[:user][:password]
+    @user.password_confirmation =params[:user][:password_confirmation]
+
+#if @user.errors.blank?
+
+#           @user.save
+
+#           redirect_to index_path
+#       else
+#           render :action => "new"
+#       end
+
+if resource.save
+    yield resource if block_given?
+    if resource.active_for_authentication?
+        set_flash_message :notice, :signed_up if is_flashing_format?
+        sign_up(resource_name, resource)
+        respond_with resource, :location => after_sign_up_path_for(resource)
+        else
+        set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_flashing_format?
+        expire_data_after_sign_in!
+        respond_with resource, :location => after_inactive_sign_up_path_for(resource)
+    end
+    else
+    clean_up_passwords resource
+    respond_with resource
 end
 
-def create
 
-@user = User.new
-@user.nome = params[:user][:nome]
-@user.cognome = params[:user][:cognome]
-#@user.datanascita = params[:user][:datanascita]
-years=params[:user]['datanascita(1i)'];
-month=params[:user]['datanascita(2i)'];
-days=params[:user]['datanascita(3i)'];
-d=Date.new(years.to_i,month.to_i,days.to_i)
-@user.datanascita = d#params[:user][:datanascita]
-
-@user.paese = params[:user][:paese]
-@user.prov = params[:user][:prov]
-@user.cap = params[:user][:cap]
-@user.via = params[:user][:via]
-@user.ncivico = params[:user][:ncivico]
-@user.email = params[:user][:email]
-@user.password = params[:user][:password]
-@user.password_confirmation =params[:user][:password_confirmation]
-
-if @user.errors.blank?
-
-@user.save
-
-redirect_to index_path
-else
-render :action => "new"
-end
-end
+    end
 
 end
